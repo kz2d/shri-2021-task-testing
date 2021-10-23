d47b479d5be77723d7006ef5c675ae5bff6805ed
kz2d <avidex111@gmail.com>
2021-10-23 17:45:08 +0300

    test tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
--- test/unit/rotesClient.test.tsx
+++ test/unit/rotesClient.test.tsx
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import {createMemoryHistory} from "history"
 import {ExampleApi,CartApi} from "../../src/client/api"
 import React from 'react';
@@ -5,7 +8,7 @@ import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {render} from "@testing-library/react"
 import { Application } from '../../src/client/Application';
-import { initStore } from '../../src/client/store';
+import { initStore } from '../../src/client/store';import "@testing-library/jest-dom";
 
 describe("open main page",()=>{
     it("open main page",()=>{
@@ -30,36 +33,11 @@ const basename = '/hw/store';
 
 const {getByText}=render(application)
 
-expect(getByText(/welcom to exemple store!/i).textContent).toEqual("Welcome t oExemple store")
+expect(getByText(/Welcome to Example store!/i).textContent).toEqual("Welcome to Example store!")
     })
 })
 
-describe("open main page",()=>{
-    it("open main page",()=>{
-        let history=createMemoryHistory({
-            initialEntries:['/'],initialIndex:0
-        })
 
-const basename = '/hw/store';
-
-
-        const api = new ExampleApi(basename);
-        const cart = new CartApi();
-        const store = initStore(api, cart);
-        
-        const application = (
-            <Router history={history}>
-                <Provider store={store}>
-                    <Application />
-                </Provider>
-            </Router>
-        );
-
-const {getByText}=render(application)
-
-expect(getByText(/welcom to exemple store!/i).textContent).toEqual("Welcome t oExemple store")
-    })
-})
 
 describe("open catalog page",()=>{
     it("open catalog page",()=>{
@@ -67,7 +45,7 @@ describe("open catalog page",()=>{
             initialEntries:['/catalog'],initialIndex:0
         })
 
-const basename = '/hw/store';
+        const basename = '/hw/store';
 
 
         const api = new ExampleApi(basename);
@@ -84,9 +62,8 @@ const basename = '/hw/store';
 
 const {getByRole}=render(application)
 
-expect(getByRole('heading',{name:/catalog!/i}).textContent).toEqual("Catalog")
-        
-})
+expect(getByRole('heading',{name:/catalog/i}).textContent).toEqual("Catalog")
+        })
 })
 
 describe("open catalog page",()=>{
@@ -112,11 +89,7 @@ const basename = '/hw/store';
 
 const {getByRole}=render(application)
 
-expect(getByRole('heading',{name:/delivery!/i}).textContent).toEqual("Delivery")
-    })
-})
-
-expect(getByRole('heading',{name:/catalog!/i}).textContent).toEqual("Catalog")
+expect(getByRole('heading',{name:/delivery/i}).textContent).toEqual("Delivery")
     })
 })
 
@@ -143,6 +116,84 @@ const basename = '/hw/store';
 
 const {getByRole}=render(application)
 
-expect(getByRole('heading',{name:/contacts!/i}).textContent).toEqual("Contacts")
+expect(getByRole('heading',{name:/contacts/i}).textContent).toEqual("Contacts")
+    })
+})
+
+describe("open cart page",()=>{
+    it("open cart page",()=>{
+        let history=createMemoryHistory({
+            initialEntries:['/cart'],initialIndex:0
+        })
+
+const basename = '/hw/store';
+
+
+        const api = new ExampleApi(basename);
+        const cart = new CartApi();
+        const store = initStore(api, cart);
+        
+        const application = (
+            <Router history={history}>
+                <Provider store={store}>
+                    <Application />
+                </Provider>
+            </Router>
+        );
+
+const {getByRole}=render(application)
+
+expect(getByRole('heading',{name:/Shopping cart/i}).textContent).toEqual("Shopping cart")
+    })
+
+    it("exmpty cart page should have link",()=>{
+        let history=createMemoryHistory({
+            initialEntries:['/cart'],initialIndex:0
+        })
+
+const basename = '/hw/store';
+
+
+        const api = new ExampleApi(basename);
+        const cart = new CartApi();
+        const store = initStore(api, cart);
+        
+        const application = (
+            <Router history={history}>
+                <Provider store={store}>
+                    <Application />
+                </Provider>
+            </Router>
+        );
+
+const {getAllByRole}=render(application)
+
+expect(getAllByRole('link').map((el)=>el.textContent)).toContain("catalog")
+    })
+
+    it("exmpty cart page should have link",()=>{
+        let history=createMemoryHistory({
+            initialEntries:['/cart'],initialIndex:0
+        })
+
+const basename = '/hw/store';
+
+
+        const api = new ExampleApi(basename);
+        const cart = new CartApi();
+        cart.setState({0:{count:2,name:"somethingIntresting",price:123456}})
+        const store = initStore(api, cart);
+        
+        const application = (
+            <Router history={history}>
+                <Provider store={store}>
+                    <Application />
+                </Provider>
+            </Router>
+        );
+
+const {getAllByRole}=render(application)
+expect(getAllByRole('link').map((el)=>el.textContent)).toContain("Cart (1)")
+
     })
 })
